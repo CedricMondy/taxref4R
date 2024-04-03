899215c53778231d56adae7a6b739a5396d59177
CedricMondy <cedric.mondy@gmail.com>
2024-04-03 18:32:47 +0200

    add functions to get information about taxa

--- R/taxa.R
+++ R/taxa.R
@@ -13,7 +13,6 @@
 #' @return a data frame with the responses (including synonyms) matching the
 #'   search parameters
 #' @export
-#'
 #' @importFrom dplyr select everything
 #' @importFrom httr parse_url build_url GET http_status content
 #' @importFrom jsonlite fromJSON
@@ -41,3 +40,97 @@ search_taxa <- function(search_strings) {
     dplyr::select(-`_links`) %>%
     dplyr::select(referenceId, referenceName, scientificName, id, frenchVernacularName, dplyr::everything())
 }
+
+
+#' Get taxa information
+#'
+#' @param cd_nom TAXREF taxon identifier
+#'
+#' @return a data frame
+#' @export
+#'
+#' @importFrom dplyr select everything
+#' @importFrom httr GET http_status content
+#' @importFrom jsonlite fromJSON
+get_taxa <- function(cd_nom) {
+  response <- file.path(base_url, "taxa", cd_nom) %>%
+    httr::GET()
+
+  if (httr::http_status(response)$category != "Success")
+    stop("Request failed with the message : ", httr::http_status(response)$message)
+
+  response %>%
+    httr::content("text") %>%
+    jsonlite::fromJSON() %>%
+    (function(x) {
+      x[names(x) != "_links"] %>%
+        lapply(function(y) if(is.null(y)) NA else y)
+    }) %>%
+    as.data.frame() %>%
+    dplyr::select(referenceId, referenceName, scientificName, id, frenchVernacularName, dplyr::everything())
+
+}
+
+#' Get taxa classification
+#'
+#' @param cd_nom TAXREF taxon identifier
+#'
+#' @return a data frame with one row per taxon from the highest parent (first
+#'   row) to the taxon corresponding to searched cd_nom (last row)
+#' @export
+#'
+#' @importFrom dplyr select everything
+#' @importFrom httr GET http_status content
+#' @importFrom jsonlite fromJSON
+get_taxa_classification <- function(cd_nom) {
+  response <- file.path(base_url, "taxa", cd_nom, "classification") %>%
+    httr::GET()
+
+  if (httr::http_status(response)$category != "Success")
+    stop("Request failed with the message : ", httr::http_status(response)$message)
+
+  content <- response %>%
+    httr::content("text") %>%
+    jsonlite::fromJSON()
+
+  content$`_embedded`$taxa %>%
+    dplyr::select(-`_links`) %>%
+    dplyr::select(referenceId, referenceName, scientificName, id, frenchVernacularName, dplyr::everything())
+
+}
+
+#' Get taxa synonyms
+#'
+#' @param cd_nom TAXREF taxon synonyms
+#'
+#' @return a data frame with one row per synonym of the taxon corresponding to
+#'   searched cd_nom
+#' @export
+#'
+#' @importFrom dplyr select everything
+#' @importFrom httr GET http_status content
+#' @importFrom jsonlite fromJSON
+get_taxa_synonyms <- function(cd_nom) {
+  cd_ref <- get_taxa(cd_nom)$referenceId
+
+  response <- file.path(base_url, "taxa", cd_ref, "synonyms") %>%
+    httr::GET()
+
+  if (httr::http_status(response)$category != "Success")
+    stop("Request failed with the message : ", httr::http_status(response)$message)
+
+  content <- response %>%
+    httr::content("text") %>%
+    jsonlite::fromJSON()
+
+  if (is.null(content$`_embedded`$taxa)) {
+    cat("No synonym found")
+  } else {
+    content$`_embedded`$taxa %>%
+      dplyr::select(-`_links`) %>%
+      dplyr::bind_rows(get_taxa(cd_ref), .) %>%
+      dplyr::select(fullName, id, referenceName, referenceId, frenchVernacularName, dplyr::everything())
+  }
+}
+
+
