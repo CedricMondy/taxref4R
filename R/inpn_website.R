c09b884114a702cca91e842fc975a17a4e0f25c1
CedricMondy <cedric.mondy@gmail.com>
2024-11-08 14:11:28 +0100

    add a function to get taxa picture from INPN

--- R/inpn_website.R
+++ R/inpn_website.R
@@ -5,10 +5,10 @@
 #' @return a data frame
 #' @export
 #'
-#' @importFrom dplyr mutate
+#' @importFrom dplyr select mutate
 #' @importFrom httr GET
 #' @importFrom rvest read_html html_node html_table
-get_habitats <- function(cd_nom) {
+get_taxa_habitats <- function(cd_nom) {
   cd_ref <- get_taxa(cd_nom)$referenceId
 
   habitat_url <- paste0("https://inpn.mnhn.fr/espece/cd_nom/", cd_ref, "/tab/habitats")
@@ -24,4 +24,26 @@ get_habitats <- function(cd_nom) {
       dplyr::select(-1) %>%
       dplyr::mutate(cd_ref = cd_ref)
   }
+}
+
+#' Title
+#'
+#' @param cd_nom
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' @importFrom httr GET
+#' @importFrom rvest html_attr html_node read_html
+get_taxa_photo <- function(cd_nom) {
+  cd_ref <- get_taxa(cd_nom)$referenceId
+
+  portrait_url <- paste0("https://inpn.mnhn.fr/espece/cd_nom/", cd_ref, "/tab/fiche")
+
+  photo_url <- portrait_url |>
+    httr::GET() |>
+    rvest::read_html() |>
+    rvest::html_node("div > a > figure > img") |>
+    rvest::html_attr("src")
 }
